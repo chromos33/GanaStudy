6edb9804b2c094894994cb017325c8ba179be608
Benjamin Kreft <benjamin@kreft.com>
2016-03-25 15:08:56 +0100

    Basic UI + Japanese Field Accepts Paint brushes and interprets it

--- GanaStudy/GanaStudy/MainPage.xaml.cs
+++ GanaStudy/GanaStudy/MainPage.xaml.cs
@@ -12,6 +12,14 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Input.Inking;
+using Windows.Media.Ocr;
+using Windows.ApplicationModel;
+using Windows.Globalization;
+using Windows.Graphics.Imaging;
+using Windows.UI.Text.Core;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Documents;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +30,70 @@ namespace GanaStudy
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private MainPage rootPage;
+        InkRecognizerContainer inkRecognizerContainer = null;
+        private IReadOnlyList<InkRecognizer> recoView = null;
+        private Language previousInputLanguage = null;
+        private CoreTextServicesManager textServiceManager = null;
+        private ToolTip recoTooltip;
+        private InkRecognizer japrecog;
         public MainPage()
         {
             this.InitializeComponent();
+            rootPage = this;
+            Gana.InkPresenter.InputDeviceTypes =
+                Windows.UI.Core.CoreInputDeviceTypes.Mouse |
+                Windows.UI.Core.CoreInputDeviceTypes.Pen;
+            InkDrawingAttributes drawingAttributes = new InkDrawingAttributes();
+            Gana.InkPresenter.StrokesCollected += InkPresenter_StrokesCollected;
+            drawingAttributes.Color = Windows.UI.Colors.Black;
+            drawingAttributes.IgnorePressure = false;
+            drawingAttributes.FitToCurve = true;
+            Gana.InkPresenter.UpdateDefaultDrawingAttributes(drawingAttributes);
+            inkRecognizerContainer = new InkRecognizerContainer();
+            recoView = inkRecognizerContainer.GetRecognizers();
+            if (recoView.Count() > 0)
+            {
+                foreach (InkRecognizer recognizer in recoView)
+                {
+                    if (recognizer.Name == "Microsoft 日本語手書き認識エンジン")
+                    {
+                        inkRecognizerContainer.SetDefaultRecognizer(recognizer);
+                    }
+                }
+            }
+            else
+            {
+                var dialog = new MessageDialog("Please Install Japanese Handwriting");
+                MessageBox(dialog);
+            }
+        }
+        private async void InkPresenter_StrokesCollected(InkPresenter sender, InkStrokesCollectedEventArgs args)
+        {
+            IReadOnlyList<InkStroke> currentStrokes = Gana.InkPresenter.StrokeContainer.GetStrokes();
+            if (currentStrokes.Count > 0)
+            {
+
+                var recognitionResults = await inkRecognizerContainer.RecognizeAsync(Gana.InkPresenter.StrokeContainer, InkRecognitionTarget.All);
+
+                if (recognitionResults.Count > 0)
+                {
+                    // Display recognition result
+                    string str = "";
+                    foreach (var r in recognitionResults)
+                    {
+                        str += r.GetTextCandidates()[0];
+                    }
+                    preview.Text = str;
+                }
+            }
+
+        }
+
+
+        private async void MessageBox(MessageDialog dialog)
+        {
+            await dialog.ShowAsync();
         }
     }
 }
